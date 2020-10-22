e894cf7c40d15221c96011a45a6f9e4ad614ffe6
崔宫健 <45160851+cuigongjian2000@users.noreply.github.com>
2020-10-22 19:21:22 +0800

    Add files via upload

---
+++ precache-manifest.1674aa20c9795974ba3498b8fd842914.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7a47ba4b290e864c866c6e54d6e9c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb7a2bfc6f88c331210",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a9251b91b145b00fac76",
+    "url": "/static/css/main.d84986b7.chunk.css"
+  },
+  {
+    "revision": "3cb7a2bfc6f88c331210",
+    "url": "/static/js/2.01bba909.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.01bba909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9251b91b145b00fac76",
+    "url": "/static/js/main.155d4257.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "91f1ade9d746c9fb29ff2b9638add467",
+    "url": "/static/media/0.jpg_.91f1ade9.webp"
+  },
+  {
+    "revision": "9e821e1dc72ee02da29b923927580df9",
+    "url": "/static/media/1.9e821e1d.jpg"
+  },
+  {
+    "revision": "c7767f91e2f2a6f4cd734d2797aacc71",
+    "url": "/static/media/2.c7767f91.jpg"
+  },
+  {
+    "revision": "f34e7cba16aad747c8dab3823c8a8f3b",
+    "url": "/static/media/3.f34e7cba.jpg"
+  },
+  {
+    "revision": "6c2e8bbb0a6c0f33276869095a424bd3",
+    "url": "/static/media/4.6c2e8bbb.jpg"
+  },
+  {
+    "revision": "872bc7556ac42717080143f8330b3241",
+    "url": "/static/media/5.872bc755.jpg"
+  },
+  {
+    "revision": "6405a95dfe3c35a5855a85dcc699fbea",
+    "url": "/static/media/T.6405a95d.jpg"
+  },
+  {
+    "revision": "8703f2de59cf9d6fae5f71958e7bbe24",
+    "url": "/static/media/banner.8703f2de.jpg"
+  },
+  {
+    "revision": "6405a95dfe3c35a5855a85dcc699fbea",
+    "url": "/static/media/s.6405a95d.jpg"
+  },
+  {
+    "revision": "6b55396ec634fbe26567a34c9751831f",
+    "url": "/static/media/taobao.6b55396e.PNG"
+  },
+  {
+    "revision": "fa8f8cba50eeb821551f1d30ba89701d",
+    "url": "/static/media/w.fa8f8cba.jpg"
+  }
+]);
